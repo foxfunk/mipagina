0b7e6c1e98fa4ddc3956c14b23ba20bd8a183e21
foxfunk <foxheyer@hotmail.com>
2013-05-17 10:30:32 -0400

    mi pagina

---
+++ mis proyectos en rails/mipagina/mipagina/app/models/example.rb
@@ -0,0 +1,4 @@
+class Example < ActiveRecord::Base
+  # attr_accessible :title, :body
+  attr_accessible :nombre
+end
---
+++ mis proyectos en rails/mipagina/mipagina/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mipagina::Application.config.secret_token = 'ebdae3b0eed3c7019e9f7473b6979a2769addf9264fc1206ec7d75dfd31734105f44cdc75f4c28f1860c7760d8779fb2e193d2256e8dcf48cdb63a956f65bee3'
