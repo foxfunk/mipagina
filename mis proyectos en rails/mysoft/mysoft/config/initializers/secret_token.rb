e5e8b0aa7f49083f224f6c4b92d71fd5e15c0fa6
foxfunk <foxheyer@hotmail.com>
2013-05-17 10:39:09 -0400

    my web

---
+++ mis proyectos en rails/mysoft/mysoft/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mysoft::Application.config.secret_token = '33ab40964b80b61b6ba940a576397706b433086d2cce2796133e621a81adff88764176e3585abafb3f048ae726ac95a92eaa222b879096998e93bc6c1b4ac993'
