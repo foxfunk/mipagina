c047e2b597ab83dcabccaf7ad1b4b71c7f3060e7
foxfunk <foxheyer@hotmail.com>
2013-05-18 20:49:30 -0400

    jugando con git y rails

---
+++ paginaruby/mypage/mypage/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mypage::Application.config.secret_token = '129981606ca7d5f4718d9659372de937a9bde18dd4ac4b8d9f01f1b7c44568d123671bfb717583ad060538e264782465494343d7838b2e51ae6fbf388f1eb300'
